aafcd2649436763bb2d315f6e0d59567a521e3e1
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-10-14 00:49:15 -0400

    Add more

---
+++ scripts/main.ts
@@ -0,0 +1,6 @@
+import "swiper"
+import "boxicons";
+import smoothscroll from "smoothscroll-polyfill";
+
+// kick off the polyfill!
+smoothscroll.polyfill();
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    clearScreen: false,
+    appType: "custom",
+    publicDir: resolve("public"),
+    build: {
+        lib: {
+            fileName: "script",
+            entry: resolve("scripts", "main.ts"),
+            formats: ["cjs"],
+        },
+        outDir: resolve("public"),
+        emptyOutDir: false,
+        assetsDir: "",
+    },
+    server: {
+        port: 8001,
+    },
+})
+;
